0f938e34da85e17373665154c10172f1aa0f9957
MarinaSigida <sigida.web@gmail.com>
2024-09-12 15:57:30 +0200

    add swiper on flavors

--- src/components/HomePage/Flavors.jsx
+++ src/components/HomePage/Flavors.jsx
@@ -7,10 +7,38 @@ const Flavors = () => {
         NOS <span>GOÛTS</span>
       </h1>
       <div className="flavors-list">
-        <Flavor />
-        <Flavor />
-        <Flavor />
-        <Flavor />
+        <swiper-container
+          pagination="dynamic-bullets"
+          navigation="true"
+          space-between="18"
+          slides-per-view="auto"
+          style={{
+            width: '100%',
+            padding: '0 30px',
+            '--swiper-pagination-color': '#9F233E',
+            '--swiper-pagination-bottom': '0',
+            '--swiper-theme-color': '#9F233E',
+          }}
+        >
+          <swiper-slide>
+            <Flavor />
+          </swiper-slide>
+          <swiper-slide>
+            <Flavor />
+          </swiper-slide>
+          <swiper-slide>
+            <Flavor />
+          </swiper-slide>
+          <swiper-slide>
+            <Flavor />
+          </swiper-slide>
+          <swiper-slide>
+            <Flavor />
+          </swiper-slide>
+          <swiper-slide>
+            <Flavor />
+          </swiper-slide>
+        </swiper-container>
       </div>
       <div className="slider"></div>
     </section>
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { register } from 'swiper/element/bundle';
+
 import './index.css';
+
+register(); // register swiper
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
