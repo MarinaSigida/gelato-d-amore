a72123340903e9e5008d45ef0ccac812875c9111
MarinaSigida <sigida.web@gmail.com>
2024-12-10 10:35:53 +0100

    add quantity limitation to buy

--- src/components/HomePage/Flavor.jsx
+++ src/components/HomePage/Flavor.jsx
@@ -2,6 +2,7 @@ import iceCreamPlaceholder from '../../assets/images/placeholder-ice-cream.png';
 import { useDispatch } from 'react-redux';
 import { addToBasket } from '../../features/basketSlice';
 import { useState } from 'react';
+import { toast } from 'sonner';
 
 const imageKey = import.meta.env.VITE_IMAGE_KEY;
 
@@ -11,27 +12,37 @@ const Flavor = ({
   image,
   pricePerUnit,
   category,
+  quantity,
   onClick,
   onAddToBasket,
 }) => {
-  const [quantity, setQuantity] = useState(1);
+  const [quantityToBuy, setQuantityToBuy] = useState(1);
   const dispatch = useDispatch();
 
   const handleIncrement = () => {
-    setQuantity((prevQuantity) => prevQuantity + 1);
+    setQuantityToBuy((prevQuantity) => {
+      if (prevQuantity < quantity) {
+        return prevQuantity + 1;
+      } else {
+        toast.error(
+          `Vous ne pouvez pas ajouter plus de ${quantity} glaces ${title} à votre panier`
+        );
+        return prevQuantity;
+      }
+    });
   };
 
   const handleDecrement = () => {
-    if (quantity > 1) {
-      setQuantity((prevQuantity) => prevQuantity - 1);
+    if (quantityToBuy > 1) {
+      setQuantityToBuy((prevQuantity) => prevQuantity - 1);
     }
   };
 
-  const handdleAddToBasket = () => {
-    const item = { id, title, image, price: pricePerUnit, quantity };
+  const handleAddToBasket = () => {
+    const item = { id, title, image, price: pricePerUnit, quantityToBuy };
     dispatch(addToBasket(item));
     onAddToBasket(item);
-    setQuantity(1);
+    setQuantityToBuy(1);
   };
 
   return (
@@ -55,10 +66,10 @@ const Flavor = ({
 
       <div className="price-and-quantity">
         <div className="price">
-          <p>{pricePerUnit * quantity} €</p>
+          <p>{pricePerUnit * quantityToBuy} €</p>
         </div>
         <div className="quantity">
-          <p>{quantity}</p>
+          <p>{quantityToBuy}</p>
         </div>
         <div className="quantity-btn-container">
           <button className="quantity-btn" onClick={handleIncrement}>
@@ -69,7 +80,7 @@ const Flavor = ({
           </button>
         </div>
       </div>
-      <button className="buy-btn" onClick={handdleAddToBasket}>
+      <button className="buy-btn" onClick={handleAddToBasket}>
         Ajouter au panier
       </button>
     </div>
