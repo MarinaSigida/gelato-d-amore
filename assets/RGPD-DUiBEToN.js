1a6498ec3be8eb7873f0f12b75f0fa9e3641db2e
MarinaSigida <MarinaSigida@users.noreply.github.com>
2025-01-07 21:40:30 +0000

    deploy: 0efe17af35692335f6f639308ac0db1e1b991283

--- assets/RGPD-DUiBEToN.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DwZ3t2nt.js";const s=()=>r.jsx("h1",{children:"RGPD"});export{s as default};
--- assets/TermsOfSales-D7K1ejL8.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DwZ3t2nt.js";const r=()=>e.jsx("h1",{children:"Conditions générales de vente"});export{r as default};
